bef8eae6278e8712f356616a42a5a020b2e7200a
JordanRomagnoli <jordanromagnoli1999@gmail.com>
2024-03-06 15:01:24 +0100

    - ADD index index control

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,4 +14,27 @@
             </div>
         </div>
     </div>
+
+    <div class="row">
+        <div class="col-12 mb-4">
+            <h1 class="text-center text-primary">
+                I nostri progetti
+            </h1>
+        </div>
+
+        @foreach ($projects as $project)
+            <div class="col-12 col-xs-6 col-sm-4 col-md-3 mb-3">
+                <div class="card">
+                    <div class="card-body">
+                        <h4>
+                            {{ $project->title }}
+                        </h4>
+
+                        <a href="{{ route('posts.show', ['project' => $project->slug]) }}" class="btn btn-primary">
+                            More
+                        </a>
+                    </div>
+                </div>
+            </div>
+        @endforeach
 @endsection
