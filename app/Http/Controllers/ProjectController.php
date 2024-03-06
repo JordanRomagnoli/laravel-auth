92d9e6d815f3693a8fef94b6a13424fc5145fafd
JordanRomagnoli <jordanromagnoli1999@gmail.com>
2024-03-06 14:23:34 +0100

    - ADD project controller and view for Guests users

---
+++ app/Http/Controllers/ProjectController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+//Models
+use App\Models\Project;
+
+use Illuminate\Http\Request;
+
+// Helper
+use Illuminate\Support\Str;
+
+class ProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+}
