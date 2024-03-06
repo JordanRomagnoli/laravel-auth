af7797896d604419300be166850d836baee4134b
nicolaceccagnoli <nicola.ceccagnoli.94@gmail.com>
2024-03-06 14:49:32 +0100

    show-guest

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,7 +1,23 @@
 @extends('layouts.app')
 
-@section('page-title', '')
+@section('page-title', '{{ $project->title }}')
 
 @section('main-content')
+
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-body">
+                <h1 class="text-center mb-3">
+                    {{ $project->title }}
+                </h1>
+
+                <p>
+                    {{ $project->content }}
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
     
 @endsection
